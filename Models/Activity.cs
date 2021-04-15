53b50b69d76e964fa14e01449c22665930f08a79
Chris Peck <onthelan@gmail.com>
2021-04-15 16:33:19 -0400

    Updated Activity model

--- Models/Activity.cs
+++ Models/Activity.cs
@@ -4,6 +4,14 @@ namespace RazorTicket.Models
 {
     public class Activity
     {
-        
+        public int ActivityId { get; set; }
+        public string ActivityDescription { get; set; }
+        public DateTime ActivityDate { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int TicketId { get; set; }
+        public Ticket Ticket { get; set; }
     }
 }
